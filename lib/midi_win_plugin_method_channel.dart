2d34e428dbc01b5bfd8d42bd3771162f58f26020
akvus <mr.akvus@gmail.com>
2022-09-26 16:53:02 +0200

    Remove custom impl of MidiCommand

--- lib/midi_win_plugin_method_channel.dart
+++ lib/midi_win_plugin_method_channel.dart
@@ -1,25 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_midi_command_platform_interface/flutter_midi_command_platform_interface.dart';
 import 'package:flutter_midi_command_platform_interface/method_channel_midi_command.dart';
 
 void initWindows() {
-  MidiCommandPlatform.instance = MethodChannelMidiCommandWindows();
-}
-
-// TODO for now it's copied form MEthodChannelMidiCommand
-const MethodChannel _methodChannel =
-    MethodChannel('plugins.invisiblewrench.com/flutter_midi_command');
-
-class MethodChannelMidiCommandWindows extends MethodChannelMidiCommand {
-  @override
-  Future<List<MidiDevice>?> get devices async {
-    final devices =
-        await _methodChannel.invokeMethod<List<Object?>>('getDevices');
-
-    return devices!.map((e) {
-      final deviceData = (e as String).split(';');
-
-      return MidiDevice(deviceData[1], deviceData[2], deviceData[0], false);
-    }).toList();
-  }
+  MidiCommandPlatform.instance = MethodChannelMidiCommand();
 }
